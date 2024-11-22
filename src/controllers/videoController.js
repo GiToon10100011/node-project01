6be1eff96cc4004dad485cce13c9c9136b81961f
GiToon10100011 <Boon10034@gmail.com>
2024-11-22 13:13:08 +0900

    241122

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,66 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const videos = [
+  {
+    id: 1,
+    title: "First Video",
+    createdAt: "2 mins ago",
+    views: 59,
+    comments: 2,
+    ratings: 5,
+  },
+  {
+    id: 2,
+    title: "Second Video",
+    createdAt: "2 mins ago",
+    views: 49,
+    comments: 2,
+    ratings: 5,
+  },
+  {
+    id: 3,
+    title: "Third Video",
+    createdAt: "2 mins ago",
+    views: 59,
+    comments: 2,
+    ratings: 5,
+  },
+];
+export const trending = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const watch = (req, res) => {
   console.log(req.params.id);
-  res.render("watch", { pageTitle: "Watch", params: req.params.id });
+  const video = videos[req.params.id - 1];
+  res.render("watch", {
+    pageTitle: `Watching ${video.title}`,
+    video,
+  });
 };
-export const edit = (req, res) =>
-  res.send(
-    `<!DOCTYPE HTML><head><title>Node.js</title></head><body><h1>Edit Video: #${req.params.id}</h1></body>`
-  );
+export const getEdit = (req, res) =>
+  res.render("edit", {
+    pageTitle: `Editing ${videos[req.params.id - 1].title}`,
+    video: videos[req.params.id - 1],
+  });
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/video/${id}`);
+};
 export const search = (req, res) => res.send("Search Video");
-export const upload = (req, res) => res.send("Upload Video");
+export const getUpload = (req, res) => {
+  res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    id: videos.length + 1,
+    title,
+    createdAt: "Just Now",
+    views: 0,
+    comments: 0,
+    ratings: 0,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ import { userRouter } from "./routers/userRouter";
 import { videoRouter } from "./routers/videoRouter";
 const PORT = 4000;
 const app = express();
-const log = morgan("combined");
+const log = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(log);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/video", videoRouter);
